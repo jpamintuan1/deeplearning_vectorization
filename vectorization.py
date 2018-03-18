d96a2be02b29a12c6aaab2c906f65fc00de04e28
jpamintuan1 <jpamintuan1@gmail.com>
2018-03-18 08:53:25 -0700

    first

---
+++ vectorization.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 18 08:24:22 2018
+
+@author: josep
+"""
+import numpy as np
+a = np.array([1,2,3,4])
+print(a)
+
+import time
+
+a = np.random.rand(1000000)
+b = np.random.rand(1000000)
+
+tic = time.time()
+c = np.dot(a,b)
+toc = time.time()
+
+print(c)
+print("Vectorized version:" + str(1000*(toc-tic)) +"ms")
+
+c = 0
+tic = time.time()
+for i in range(1000000):
+    c+= a[i]*b[i]
+toc = time.time()
+
+print(c)
+print("For loop:" + str(1000*(toc-tic)) + "ms")
+
+
+
+
+
